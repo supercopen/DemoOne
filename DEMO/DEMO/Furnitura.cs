eac8340e47a023d23c9ef89ea2ef16a1853f9c6f
Nikolay <supercopen@yandex.ru>
2019-04-29 08:35:49 +0300

    Таблицы ткань и фурнитура!

---
+++ DEMO/DEMO/Furnitura.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DEMO
+{
+    public partial class Furnitura : Form
+    {
+        SqlConnection ConnectionString = new SqlConnection("Data Source = (Local);Initial Catalog = Demo_ex;Integrated Security = True");
+
+        public Furnitura()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form klad = new Kladovshik();
+            klad.Show();
+            this.Close();
+        }
+
+        private void Furnitura_Load(object sender, EventArgs e)
+        {
+            String query = "Select * from Фурнитура";
+
+            SqlDataAdapter sda = new SqlDataAdapter(query, ConnectionString);
+            DataSet ds = new DataSet();
+
+            sda.Fill(ds, "Фурнитура");
+
+            dataGridView1.DataSource = ds.Tables["Фурнитура"];
+        }
+    }
+}
---
+++ DEMO/DEMO/tkani.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DEMO
+{
+    public partial class tkani : Form
+    {
+        SqlConnection ConnectionString = new SqlConnection("Data Source = (Local);Initial Catalog = Demo_ex;Integrated Security = True");
+
+        public tkani()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form klsd = new Kladovshik();
+            klsd.Show();
+            this.Close();
+        }
+
+        private void tkani_Load(object sender, EventArgs e)
+        {
+            String query = "Select * from Ткань";
+
+            SqlDataAdapter sda = new SqlDataAdapter(query, ConnectionString);
+            DataSet ds = new DataSet();
+
+            sda.Fill(ds, "Ткань");
+
+            dataGridView1.DataSource = ds.Tables["Ткань"];
+        }
+    }
+}
